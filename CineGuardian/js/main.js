13608cce38267e9ddc575cf0fc9903eea5a52439
miguelsilvag <miguel.silva@sptech.school>
2023-11-06 21:32:14 -0300

    Mudança na Dashboard

--- CineGuardian/js/main.js
+++ CineGuardian/js/main.js
@@ -63,106 +63,6 @@
     Chart.defaults.color = "#6C7293";
     Chart.defaults.borderColor = "#000000";
 
-
-    // Worldwide Sales Chart
-    //var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
-    // var myChart1 = new Chart(ctx1, {
-    //     type: "bar",
-    //     data: {
-    //         labels: ["id-01", "id-02", "id-03", "id-04"],
-    //         datasets: [{
-    //                 label: "Minutos",
-    //                 data: [15, 30, 55, 65, 60, 80, 95],
-    //                 backgroundColor: "rgb(54,163,85, .6)"
-    //             },
-    //         ]
-    //         },
-    //     options: {
-    //         responsive: true
-    //     }
-    // });
-
-    //alertaDisponibilidadeTempo(120)
-
-    // function alertaDisponibilidadeTempo(tempoCompraSegundos) {
-    //     var disponibilidadeTempoMedioH5 = document.getElementById("disponibilidadeTempoMedio");
-    //     var disponibilidadeTempoMedio = tempoCompraSegundos / 60;
-
-    //    // disponibilidadeTempoMedioH5.innerHTML = `Seus clientes demoram aproximadamente<br> <spam style="color: #36A355; font-size: 1.8rem">${(disponibilidadeTempoMedio).toFixed(2)} minutos</spam> para realizar a compra`;
-    // }
-
-    // Salse & Revenue Chart
-    //var ctx2 = $("#salse-revenue").get(0).getContext("2d");
-    // var myChart2 = new Chart(ctx2, {
-    //     type: "line",
-    //     data: {
-    //         labels: ["12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"],
-    //         datasets: [{
-    //                 label: "Disponibilidade",
-    //                 data: [15, 30, 55, 45, 70, 65, 85],
-    //                 backgroundColor: "rgb(54,163,85, .6)",
-    //                 fill: true
-    //             },
-    //             {
-    //                 label: "Desempenho",
-    //                 data: [99, 135, 170, 130, 190, 180, 270],
-    //                 backgroundColor: "rgba(235, 22, 22, .5)",
-    //                 fill: true
-    //             }
-    //         ]
-    //         },
-    //     options: {
-    //         responsive: true
-    //     }
-    // });
-    
-
-
-    // Single Line Chart
-    //var ctx3 = $("#line-chart").get(0).getContext("2d");
-    // var myChart3 = new Chart(ctx3, {
-    //     type: "line",
-    //     data: {
-    //         labels: [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
-    //         datasets: [{
-    //             label: "Disponibilidade / tempo médio",
-    //             fill: true,
-    //             backgroundColor: "rgba(235, 22, 22, .7)",
-    //             data: [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15]
-    //         }]
-    //     },
-    //     options: {
-    //         responsive: true
-    //     }
-    // });
-
-
-    // function graficoDemandas(){
-    //     // Single Bar Chart
-    //     var ctx4 = $("#bar-chart").get(0).getContext("2d");
-    //     var myChart4 = new Chart(ctx4, {
-    //         type: "bar",
-    //         data: {
-    //             labels: ["2º Feira", "3º Feira", "4º Feira", "5º Feira", "6º Feira", "Sábado", "Domingo"],
-    //             datasets: [{
-    //                 backgroundColor: [
-    //                     "rgba(235, 22, 22, .7)",
-    //                     "rgba(235, 22, 22, .6)",
-    //                     "rgba(235, 22, 22, .5)",
-    //                     "rgba(235, 22, 22, .4)",
-    //                     "rgba(235, 22, 22, .3)"
-    //                 ],
-    //                 data: [55, 49, 44, 24, 15],
-    //                 label: "Horas"
-    //             }]
-    //         },
-    //         options: {
-    //             responsive: true
-    //         }
-    //     });
-    // }
-
-    // Pie Chart
     var ctx5 = $("#pie-chart").get(0).getContext("2d");
     var myChart5 = new Chart(ctx5, {
         type: "pie",
@@ -190,7 +90,7 @@
         var porcentagemMaquinasH5 = document.getElementById("porcentagemMaquinas");
         var pocentagemAtivas = (maquinasAtivas/totalMaquinas)* 100
 
-        porcentagemMaquinasH5.innerHTML = `Suas máquinas tem uma<br> disponibilidade de <spam style="color: #36A355; font-size: 1.8rem">%${(pocentagemAtivas).toFixed(2)}</spam>`
+        porcentagemMaquinasH5.innerHTML = `Suas máquinas tem uma<br> disponibilidade de <spam style="color: #36A355; font-size: 1.8rem">${(pocentagemAtivas).toFixed(2)}%</spam>`
     }
 
 
